9a484f2dbbc5c308195e1d6ced7db48fe4450ca0
CristianAUnisa <c.alfano35@studenti.unisa.it>
2021-01-04 22:23:19 +0100

    Procedura di inserimento prestazione

--- Codice Sorgente/Sistema/src/control/gestioneLaboratorio/PrestazioneControl.java
+++ Codice Sorgente/Sistema/src/control/gestioneLaboratorio/PrestazioneControl.java
@@ -1,11 +1,20 @@
 package control.gestioneLaboratorio;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Response;
+
+import model.dao.PrestazioneManager;
+import model.dao.PrestazioneManagerDS;
+import model.entity.Laboratorio;
+import model.entity.Prestazione;
 
 /**
  * Servlet implementation class PrestazioneControl
@@ -14,6 +23,8 @@ import javax.servlet.http.HttpServletResponse;
 public class PrestazioneControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	static PrestazioneManager ds = new PrestazioneManagerDS();
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,15 +37,25 @@ public class PrestazioneControl extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Laboratorio laboratorio = (Laboratorio) request.getSession().getAttribute("utente");
+		String codicePrestazione = (String) request.getParameter("prestazione");
+		Prestazione prestazione = new Prestazione();
+		prestazione.setId(codicePrestazione);
+		prestazione.setLaboratorio(laboratorio.getUsername());
+		try {
+			ds.save(prestazione);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response.sendError(Response.SC_INTERNAL_SERVER_ERROR, "Problema temporaneo del server, riprova più tardi.");
+		}
+		String redirectedPage = "/homelaboratorio.jsp";
+		response.sendRedirect(request.getContextPath() + redirectedPage);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
